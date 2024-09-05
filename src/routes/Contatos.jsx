1a4b9298979d4219a5b583fa2664774677b15a8e
ton <errodrigues2006@outlook.com.br>
2024-09-05 09:16:17 -0300

    Adicionando Contatos.jsx

---
+++ src/routes/Contatos.jsx
@@ -0,0 +1,9 @@
+import ContatosStyle from "styled-components"
+const Contatos=()=>{
+    return(<>
+        <ContatosStyle>
+            <h1>Olá</h1>
+        </ContatosStyle>
+    </>)
+}
+export default Contatos

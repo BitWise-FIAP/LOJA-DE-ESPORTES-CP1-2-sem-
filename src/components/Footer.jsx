103e817ee3d78a67a7b22374b0362c981c922e96
brunobiletsky <brunobiletsky@gmail.com>
2024-09-05 09:49:57 -0300

    estilizando imagens do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
-import { FooterStyle } from "../css/FooterStyle"
+import { FooterStyle } from '../css/FooterStyle'
+import footer_apple from '../assets/footer_apple.png' 
+import footer_google from '../assets/footer_google.png' 
 
 const Footer=()=>{
     return(
         <>
         <FooterStyle>
-            <footer className="footer">
+            <footer className='footer'>
                 <h5>@2024 - Todos os direitos reservados</h5>
-                <img src="#" alt="Baixe na APPLE STORE"></img>
-                <img src="#" alt="Baixe na Google Play"></img>
+                <img src={footer_apple} className='apple' alt='Baixe na APPLE STORE'></img>
+                <img src={footer_google} className='google' alt='Baixe na Google Play'></img>
 
             </footer>
         </FooterStyle>
--- src/css/FooterStyle.jsx
+++ src/css/FooterStyle.jsx
@@ -7,9 +7,15 @@ export const FooterStyle = styled.section`
     background-color: gray;
     padding: 15px;
     justify-content: space-around;
-
 }
 .footer h5{
     font-size: 15px;
+}
+.apple{
+    height: 3.4rem;
+    margin-top: 14px;
+}
+.google{
+    height: 5rem;
 }
 `

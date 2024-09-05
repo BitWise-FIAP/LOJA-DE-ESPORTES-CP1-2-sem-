d36a87611a70f87f96176e741fa15dcedb4dbb76
brunobiletsky <brunobiletsky@gmail.com>
2024-09-05 09:05:56 -0300

    testando fundo da home

--- src/css/HomeStyle.jsx
+++ src/css/HomeStyle.jsx
@@ -3,6 +3,6 @@ import styled from "styled-components"
 export const HomeStyle = styled.section`
 
 .corpo{
-    background-image: linear-gradient: blue,white;
+    background-image: radial-gradient(circle, #5c0067 0%, #00d4ff 100%);
 }
 `

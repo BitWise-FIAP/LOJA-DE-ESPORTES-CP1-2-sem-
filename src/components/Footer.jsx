af47870014ab7c53dd116b0d9fa02716783cecf6
Pedro <pedrogasparff@gmail.com>
2024-09-06 20:27:10 -0300

    Criando link para as fotos da apple store e da google play

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,18 +2,25 @@ import { FooterStyle } from '../css/FooterStyle'
 import footer_apple from '../assets/footer_apple.png' 
 import footer_google from '../assets/footer_google.png' 
 
-const Footer=()=>{
-    return(
+const Footer = () => {
+    return (
         <>
-        <FooterStyle>
-            <footer className='footer'>
-                <h5>@2024 - Todos os direitos reservados</h5>
-                <img src={footer_apple} className='apple' alt='Baixe na APPLE STORE'></img>
-                <img src={footer_google} className='google' alt='Baixe na Google Play'></img>
+            <FooterStyle>
+                <footer className='footer'>
+                    <h5>@2024 - Todos os direitos reservados</h5>
+                    
+                    {/* Link para a Apple Store */}
+                    <a href="https://www.apple.com/app-store/" target="_blank" rel="noopener noreferrer">
+                        <img src={footer_apple} className='apple' alt='Baixe na APPLE STORE' />
+                    </a>
+                    
+                    {/* Link para a Google Play */}
+                    <a href="https://play.google.com/store" target="_blank" rel="noopener noreferrer">
+                        <img src={footer_google} className='google' alt='Baixe na Google Play' />
+                    </a>
 
-            </footer>
-        </FooterStyle>
-        
+                </footer>
+            </FooterStyle>
         </>
     )
 }

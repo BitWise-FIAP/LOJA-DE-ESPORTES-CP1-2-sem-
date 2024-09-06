697e10414d0adce10f5dfaa3390899d2771faf6b
Bruno Biletsky <brunobiletsky@gmail.com>
2024-09-06 00:22:13 -0300

    sobre nós estilizado com banner adicionado

--- src/css/SobreStyle.jsx
+++ src/css/SobreStyle.jsx
@@ -5,9 +5,34 @@ export const SobreStyle = styled.section`
     background-image: radial-gradient(circle, #1e0e64 0%, #00d4ff 100%);
     padding: 15px;
 }
+.content h1{
+    font-size: 30px;
+}
+.content img{
+    width: 100%;
+    height: 650px;
+    border-style: solid;
+    border-color: #1e0e64;
+    border-width: 5px;
+    position: absolute;
+    filter: brightness(75%);
+    
+    
+}
 .texto{
     font-size: 20px;
     font-family: cursive;
-
+    color: #00d4ff;
+    text-align: justify;
+    text-shadow: 2px 1px #1e0e64;
+    position: absolute;
+    width: 800px;
+    padding-left: 985px;
+    
+    
+}
+.sobrepor{
+    position: relative;
+    margin-bottom: 700px;
 }
 `
--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -1,26 +1,28 @@
 import { SobreStyle } from "../css/SobreStyle"
-
+import banner_sobre from "../assets/banner-sobre.jpg"
 const Sobre=()=>{
     return(
         <>
         <SobreStyle>
             <section className="content">
-                <img src="#" alt="imagem acima do texto"></img>
-                <div className="texto">
-                    <h3>A Importância de Praticar Esportes para a Saúde e Bem-Estar</h3>
-                    <p>Praticar esportes é fundamental para manter uma vida saudável e equilibrada.
-                        A atividade física regular ajuda a prevenir uma série de doenças crônicas, como diabetes, hipertensão e doenças cardiovasculares.
-                        Além disso, melhora a saúde mental, reduzindo os níveis de estresse e ansiedade, e promovendo uma sensação de bem-estar através da liberação de endorfinas.
-                        Esportes também são essenciais para o controle do peso corporal, pois aumentam o gasto calórico e ajudam a manter um metabolismo ativo.
-                        A prática esportiva fortalece os músculos e ossos, melhora a flexibilidade e a coordenação motora, e aumenta a resistência física.
-                            Outro benefício importante é a melhoria da qualidade do sono, que é essencial para a recuperação do corpo e da mente.
-                            Além dos benefícios físicos, praticar esportes promove a socialização e o desenvolvimento de habilidades como trabalho em equipe, disciplina e liderança.
-                            Aprática regular de esportes é uma das melhores formas de garantir uma vida longa, saudável e feliz.
-                            Além disso, a prática de esportes pode ser uma excelente forma de lazer e diversão.
-                                Participar de atividades esportivas em grupo ou individualmente pode proporcionar momentos de alegria e descontração,
-                                ajudando a aliviar a rotina diária e a criar memórias positivas. Esportes como futebol, basquete, natação e ciclismo,
-                                entre outros, oferecem oportunidades para explorar novos hobbies e interesses,
-                                além de incentivar um estilo de vida mais ativo e dinâmico.</p>
+                <div className="sobrepor">
+                    <img src={banner_sobre} alt="imagem acima do texto"></img>
+                    <div className="texto">
+                        <h3>A Importância de Praticar Esportes para a Saúde e Bem-Estar</h3>
+                        <p>Sobre nós: Incentivamos a prática esportiva! Praticar esportes é fundamental para manter uma vida saudável e equilibrada.
+                            A atividade física regular ajuda a prevenir uma série de doenças crônicas, como diabetes, hipertensão e doenças cardiovasculares.
+                            Além disso, melhora a saúde mental, reduzindo os níveis de estresse e ansiedade, e promovendo uma sensação de bem-estar através da liberação de endorfinas.
+                            Esportes também são essenciais para o controle do peso corporal, pois aumentam o gasto calórico e ajudam a manter um metabolismo ativo.
+                            A prática esportiva fortalece os músculos e ossos, melhora a flexibilidade e a coordenação motora, e aumenta a resistência física.
+                                Outro benefício importante é a melhoria da qualidade do sono, que é essencial para a recuperação do corpo e da mente.
+                                Além dos benefícios físicos, praticar esportes promove a socialização e o desenvolvimento de habilidades como trabalho em equipe, disciplina e liderança.
+                                Aprática regular de esportes é uma das melhores formas de garantir uma vida longa, saudável e feliz.
+                                Além disso, a prática de esportes pode ser uma excelente forma de lazer e diversão.
+                                    Participar de atividades esportivas em grupo ou individualmente pode proporcionar momentos de alegria e descontração,
+                                    ajudando a aliviar a rotina diária e a criar memórias positivas. Esportes como futebol, basquete, natação e ciclismo,
+                                    entre outros, oferecem oportunidades para explorar novos hobbies e interesses,
+                                    além de incentivar um estilo de vida mais ativo e dinâmico.</p>
+                    </div>
                 </div>
             </section>
         </SobreStyle>

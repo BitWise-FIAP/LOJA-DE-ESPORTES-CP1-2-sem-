0318339fa80fd6682df28eee35c9f76c6f9f0064
Bruno Biletsky <brunobiletsky@gmail.com>
2024-09-06 22:53:54 -0300

    aba contatos criada e estilizada

--- src/css/ContatosStyle.jsx
+++ src/css/ContatosStyle.jsx
@@ -2,4 +2,48 @@ import styled from "styled-components";
 
 export const ContatosStyle = styled.section`
 
+
+.corpo{
+    background-image: radial-gradient(circle, #1e0e64 0%, #00d4ff 100%);
+    max-width: 100%;
+    margin: 0;
+    padding: 0;
+}
+.estruturabanner{
+    position: relative;
+    width: 100%;
+}
+.bannerdiv{
+    max-width: 100%;
+    height:30rem;
+}
+.banner{
+    width: 100%;
+    position: absolute;
+    height: 30rem;
+    object-fit: none 
+}
+.bannertitle{
+    position: absolute;
+    top: 5rem;
+    left: 50%;
+    transform: translate(-50%);
+    font-size: 4rem;
+    color: white;
+    text-align: center;
+}
+.flexdiv{
+    display: flex;
+    justify-content: center;
+}
+.grid{
+    font-size: 1.25rem;
+    color:#9fdbfd;
+    text-shadow: 2px 1px #120303;
+    filter: drop-shadow(0 0 1.2rem #000000);
+    padding: 10vh;
+}
+.grid img{
+    filter: brightness(10);
+}
 `
--- src/routes/Contatos.jsx
+++ src/routes/Contatos.jsx
@@ -1,9 +1,30 @@
-import ContatosStyle from "styled-components"
+import {ContatosStyle} from "../css/ContatosStyle"
+import banner_contato from "../assets/banner-contato.jpg"
+import fale_conosco from "../assets/fale-conosco.png"
 const Contatos=()=>{
-    return(<>
+    return(
+        <>
         <ContatosStyle>
-            <h1>Olá</h1>
+            <section className="corpo">
+                <div className="estruturabanner">
+                        <div className="bannerdiv">
+                            <img className="banner" src={banner_contato}></img>
+                            <h1 className="bannertitle">CONTATO</h1>
+                            <h5 className="bannertitle"><br></br>Prezamos pelo melhor atendimento!</h5>
+                        </div>
+                        <div className="flexdiv">
+                    <div className="grid">
+                        <img src={fale_conosco}></img>
+                    </div>
+                    <div className="grid">
+                        <h1>Fale Conosco: lojaesportesbitwise@email.com</h1>
+                        <h3>Telefone: (11) 2222-1000</h3>
+                    </div>   
+                </div>  
+                    </div>
+            </section>
         </ContatosStyle>
-    </>)
+        </>
+    )
 }
 export default Contatos
